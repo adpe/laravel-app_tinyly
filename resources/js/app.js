abe46e9ecf989e6dc7247e1b583c0fac34308f90
Adrian Perez <me@adrianperez.me>
2021-02-10 08:14:17 +0100

    Auto fade-out alert-toast after 5s

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+var toast = document.querySelector('.alert-toast input');
+if (toast !== null) {
+    window.addEventListener('load', function() {
+        setTimeout(function(){
+            toast.checked = true;
+        }, 5000);
+    });
+}
