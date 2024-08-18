aef12cd43ad2640d502a2a1a3787b29ce6b474f2
Adrian Perez <me@adrianperez.me>
2024-08-18 10:58:00 +0200

    Fix failing example feature test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/links');
+    if (auth()->check()) {
+        return redirect('/links');
+    }
+
+    return view('welcome');
 });
 
 Route::group(['middleware' => 'auth'], function () {
